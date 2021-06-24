58b1cc30dfdb91f2071d2b4f52a01c6f4af0bad7
Santiago Curi <curisantiago3@gmail.com>
2021-06-23 21:04:15 -0300

    window

---
+++ Shaders/bloom_final.fs
@@ -0,0 +1,29 @@
+ #version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D scene;
+uniform sampler2D bloomBlur;
+uniform bool bloom;
+uniform float exposure;
+uniform float transparent;
+
+
+
+void main()
+{             
+  const float gamma = 0.5;
+  vec3 hdrColor = texture2D(scene, vec2(gl_FragCoord.x/1024, gl_FragCoord.y/720)).rgb;      
+  vec3 bloomColor = texture2D(bloomBlur, vec2(gl_FragCoord.x/1024, gl_FragCoord.y/720)).rgb;
+
+  if(bloom)
+     hdrColor += bloomColor; // additive blending
+
+  // tone mapping
+  vec3 result = vec3(1.0) - exp(-hdrColor * exposure);
+
+  // also gamma correct while we're at it       
+  result = pow(result, vec3(1.0 / gamma));
+  FragColor = vec4(result, transparent);
+}
