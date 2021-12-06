4273f3288744571e2eacea1eec36bb17f2615163
Gustavo <gusta97_2009@hotmail.com>
2021-12-06 12:07:00 -0300

    ad distance lights

--- Shaders/model_loading.fs
+++ Shaders/model_loading.fs
@@ -112,7 +112,10 @@ void main()
       }
 
       for(int i = 0; i < NR_SPOT_LIGHTS; i++){
-         result += CalcSpotLight(spotLights[i], norm, FragPos, viewDir);
+         float distance = length(spotLights[i].position - viewPos);
+         if(distance < 8f){
+              result += CalcSpotLight(spotLights[i], norm, FragPos, viewDir);
+         }
       }
 
       
